ce2f39993841d1c610166b23d9d94b995f3bcfdd
sayamcodezzz <sayamkhajuria@gmail.com>
2026-07-17 23:32:25 +0530

    fixing ai model issue

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,15 +1,19 @@
-import { GoogleGenAI } from "@google/genai";
+const  { GoogleGenAI } = require("@google/genai");
 
 const ai = new GoogleGenAI({
     apiKey:process.env.GOOGLE_GENAI_API_KEY,
 });
+async function invokeGeminiAi() {
+  try {
+    const response = await ai.models.generateContent({
+      model: "models/gemini-3.1-flash-lite",
+      contents: "Explain how AI works in a few words",
+    });
 
-  async function invokeGeminiAi(){
-const response = await ai.models.generateContent({
-  model: "gemini-2.5-flash",
-  contents: "Explain how AI works in a few words",
-});
-console.log(response.text);
- }
- 
+    console.log(response.text);
+  } catch (err) {
+    console.error(err.message);
+    console.error(err);
+  }
+}
  module.exports=invokeGeminiAi;
